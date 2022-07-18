0ece421ebc0b6235d4a4a93070736f90f6c34dae
Darshan <mr.darshan2001@gmail.com>
2022-07-18 13:35:31 +0530

    Darshan's branch

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('registration', views.registration, name='registration' ),
     path('login', views.loginuser, name='login' ),
     path('logout', views.logoutuser, name='logout' ),
+    path('selected_blog', views.selected_blog,name='selected_blog'),
 ]
