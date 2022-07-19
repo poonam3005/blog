9ff65da55ce69a02620337dcee72fdaea08a79e8
Darshan <mr.darshan2001@gmail.com>
2022-07-19 16:19:00 +0530

    Add Post Blog

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,4 @@
+from turtle import mode
 from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ImageField
@@ -21,10 +22,21 @@ class Blog(models.Model):
     author = models.ForeignKey(User,  on_delete=models.CASCADE)
     title = models.ForeignKey(Title,  on_delete=models.CASCADE, blank=True, null=True)
     category = models.ForeignKey(Category,  on_delete=models.CASCADE, blank=True, null=True)
+    entryTxt = models.CharField(max_length=300,blank=True,null=True)
     Desc =  HTMLField(blank=True, null=True)
     keyword = models.CharField(max_length=50, blank=True, null=True)
     image = models.ImageField(upload_to='static', null=True,blank=True)
     date = models.DateField( auto_now=True)
+    likes = models.IntegerField(null=True,blank=True)
 
     def __str__(self) :
         return str(self.id)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Blog,related_name='comments',on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    body = models.TextField()
+    date_added = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return '%s-%s' % (self.post.title,self.name)
--- blogapp/urls.py
+++ blogapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('registration', views.registration, name='registration' ),
     path('login', views.loginuser, name='login' ),
     path('logout', views.logoutuser, name='logout' ),
-    path('selected_blog', views.selected_blog,name='selected_blog'),
+    path('selected_blog/<id>', views.selected_blog,name='selected_blog'),
+    path('upload-blog',views.upload_blog,name='upload-blog')
 ]
